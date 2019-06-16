22de520ccda052f35347f1b6ca3e723c895ec317
justin <jlouie10@gmail.com>
2019-06-16 10:41:58 -0500

    Db setup

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+  DROP DATABASE IF EXISTS burgers_db;
+
+  CREATE DATABASE burgers_db;
+
+  USE burgers_db;
+
+  CREATE TABLE burgers (
+    `burger_id` INT NOT NULL AUTO_INCREMENT,
+    `burger_name` VARCHAR(255) NOT NULL,
+    `devoured` BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (`burger_id`)
+  );
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (`burger_name`)
+VALUES ('Old Fashioned'),
+    ('Cheeseburger'),
+    ('Impossible Burger');
