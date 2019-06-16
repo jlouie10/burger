3907556c7ebd6c1f16bbdba6f935d30dd6e7f1b6
justin <jlouie10@gmail.com>
2019-06-16 14:47:40 -0500

    Added Express Handlebars configuation to server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+
+let app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
